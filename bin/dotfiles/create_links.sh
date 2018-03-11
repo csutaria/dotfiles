b222ff1e26f73caf4b2f519b4c30975295d6d9b6
Cyrus Sutaria <cyfive1@gmail.com>
2018-03-11 01:40:44 -0800

    Create .zfunctions directory before linking

--- bin/dotfiles/create_links.sh
+++ bin/dotfiles/create_links.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+mkdir -p $HOME/.zfunctions
 ln -s $HOME/repositories/pure/async.zsh $HOME/.zfunctions/async
 ln -s $HOME/repositories/pure/pure.zsh $HOME/.zfunctions/prompt_pure_setup
 
