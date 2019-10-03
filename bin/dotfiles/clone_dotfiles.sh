1c88cd13415044f88bd4fe0aa389b6b37f6a06f7
Cyrus Sutaria <cysut007@gmail.com>
2019-10-03 15:57:41 +0000

    Change location of pure prompt and enable alias in clone_dotfiles.sh

--- bin/dotfiles/clone_dotfiles.sh
+++ bin/dotfiles/clone_dotfiles.sh
@@ -2,6 +2,8 @@
 
 mkdir -p $HOME/repositories/dotfiles
 
+# Honor aliases
+shopt -s expand_aliases
 alias config='/usr/bin/git --git-dir=$HOME/repositories/dotfiles/ --work-tree=$HOME'
 
 git clone --bare https://github.com/csutaria/dotfiles.git $HOME/repositories/dotfiles
