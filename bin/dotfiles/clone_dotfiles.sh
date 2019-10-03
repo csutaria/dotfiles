92643066ef6f5532265b57ece4e0f9200ad97719
Cyrus Sutaria <cysut007@gmail.com>
2019-10-03 07:12:29 +0000

    Update the script to clone dotfiles, and remove pure git module

--- bin/dotfiles/clone_dotfiles.sh
+++ bin/dotfiles/clone_dotfiles.sh
@@ -2,8 +2,9 @@
 
 mkdir -p $HOME/repositories/dotfiles
 
-# alias config='/usr/bin/git --git-dir=$HOME/repositories/dotfiles/ --work-tree=$HOME'
+alias config='/usr/bin/git --git-dir=$HOME/repositories/dotfiles/ --work-tree=$HOME'
 
-# Recursively clones
-git clone --recursive --bare git@github.com:csutaria/dotfiles.git $HOME/repositories/dotfiles
+git clone --bare https://github.com/csutaria/dotfiles.git $HOME/repositories/dotfiles
+config checkout
 
+config config --local status.showUntrackedFiles no
