e206779ab1f30eb71f27dbc89ccda6f49d73e11c
Cyrus Sutaria <sutaria.cyrus@gmail.com>
2020-06-04 21:41:43 -0700

    Update the way Pure prompt is set up

--- bin/dotfiles/clone_dotfiles.sh
+++ bin/dotfiles/clone_dotfiles.sh
@@ -10,3 +10,7 @@ git clone --bare https://github.com/csutaria/dotfiles.git $HOME/repositories/dot
 config checkout
 
 config config --local status.showUntrackedFiles no
+
+mkdir -p $HOME/.zfunctions
+git clone https://github.com/sindresorhus/pure.git "$HOME/.zfunctions/pure"
+
--- bin/dotfiles/create_links.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir -p $HOME/.zfunctions
-ln -s $HOME/repositories/pure/async.zsh $HOME/.zfunctions/async
-ln -s $HOME/repositories/pure/pure.zsh $HOME/.zfunctions/prompt_pure_setup
-
